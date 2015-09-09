7df63d003da6874e3698f6676499bbf68e2492a4
Alim <alimjan@ifresco.nl>
2015-09-09 14:51:29 +0200

    #3 Add 'twig' template

--- vendor/composer/autoload_namespaces.php
+++ vendor/composer/autoload_namespaces.php
@@ -6,6 +6,7 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
+    'Twig_' => array($vendorDir . '/twig/twig/lib'),
     'Symfony\\Component\\Finder\\' => array($vendorDir . '/symfony/finder'),
     'Symfony\\Component\\Console\\' => array($vendorDir . '/symfony/console'),
     'Luracast\\Restler' => array($vendorDir . '/restler/framework'),
